e04734e653af84d253ae3aeb6d10e2bee5c8b4d4
kimsunjae <woal9844@gmail.com>
2023-12-05 01:11:14 +0900

    feat : 캔버스 영역 캡쳐 기능 구현

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,11 @@
 import React, { useState, useRef } from 'react'
 import CanvasDraw from 'react-canvas-draw'
 import styled from 'styled-components'
+import html2canvas from 'html2canvas'
 
 function MainPage() {
   const [drawingMode, setDrawingMode] = useState(true)
-  const canvasRef = useRef(null)
+  const canvasRef = useRef<CanvasDraw | null>(null)
 
   const handleDrawButtonClick = () => {
     setDrawingMode(true)
@@ -15,7 +16,26 @@ function MainPage() {
   }
 
   const handleCanvasClick = () => {
-    // No need to add any logic here when using react-canvas-draw
+    if (canvasRef.current) {
+      // CanvasDraw 컴포넌트가 렌더링되는 DOM 요소를 캡처
+      const canvasElement = document.getElementById('canvas-draw')
+
+      if (canvasElement) {
+        html2canvas(canvasElement).then((canvas) => {
+          const drawingDataURL = canvas.toDataURL('image/png')
+
+          // 데이터 URL을 다운로드할 수 있는 링크를 생성합니다.
+          const downloadLink = document.createElement('a')
+          downloadLink.href = drawingDataURL
+          downloadLink.download = 'drawing.png'
+
+          // 링크를 클릭하여 다운로드를 시작합니다.
+          document.body.appendChild(downloadLink)
+          downloadLink.click()
+          document.body.removeChild(downloadLink)
+        })
+      }
+    }
   }
 
   return (
@@ -25,14 +45,17 @@ function MainPage() {
         <ToolBar>
           <DrawButton onClick={handleDrawButtonClick}>그리기</DrawButton>
           <EraseButton onClick={handleEraseButtonClick}>지우기</EraseButton>
+          <SubmitButton onClick={handleCanvasClick}>그림 전송</SubmitButton>
         </ToolBar>
-        <CanvasDraw
-          ref={canvasRef}
-          brushColor={drawingMode ? 'black' : 'white'}
-          style={{ border: '2px solid #333' }}
-          canvasWidth={1800}
-          canvasHeight={800}
-        />
+        <Canvas id="canvas-draw">
+          <CanvasDraw
+            ref={canvasRef}
+            brushColor={drawingMode ? 'black' : 'white'}
+            style={{ border: '2px solid #333' }}
+            canvasWidth={1800}
+            canvasHeight={800}
+          />
+        </Canvas>
       </Container>
     </AppContainer>
   )
@@ -73,6 +96,8 @@ const ToolBar = styled.div`
   margin-right: 100px;
 `
 
+const Canvas = styled.div``
+
 const DrawButton = styled.button`
   background-color: #4caf50;
   color: white;
@@ -93,9 +118,23 @@ const EraseButton = styled.button`
   padding: 10px 20px;
   font-size: 1em;
   border: none;
+  margin-bottom: 10px;
   cursor: pointer;
 
   &:hover {
     background-color: #d32f2f;
+  }
+`
+
+const SubmitButton = styled.button`
+  background-color: #8f36f4;
+  color: white;
+  padding: 10px 20px;
+  font-size: 1em;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #d32fd3;
   }
 `
