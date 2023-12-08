7fb1a8769cf8f678375fa81eb23a54db787a3c85
kimsunjae <woal9844@gmail.com>
2023-12-09 01:54:38 +0900

    feat : (response data)png file rendering

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,15 +1,15 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import CanvasDraw from 'react-canvas-draw'
 import styled from 'styled-components'
 import html2canvas from 'html2canvas'
 import convert from '../assets/convert.png'
 import erase from '../assets/erase.png'
 import draw from '../assets/draw.png'
-import cat1 from '../assets/cat1.png'
-import cat2 from '../assets/cat2.png'
-import cat3 from '../assets/cat3.png'
-import cat4 from '../assets/cat4.png'
-import cat5 from '../assets/cat5.png'
+// import cat1 from '../assets/cat1.png'
+// import cat2 from '../assets/cat2.png'
+// import cat3 from '../assets/cat3.png'
+// import cat4 from '../assets/cat4.png'
+// import cat5 from '../assets/cat5.png'
 import addFeel from '../assets/addFeel.png'
 
 function MainPage() {
@@ -18,6 +18,10 @@ function MainPage() {
   // MainPage 컴포넌트 내부에서 useState 추가
   const [brushSize, setBrushSize] = useState(5) // 초기값은 5로 설정
 
+  // 받은 이미지를 저장할 상태 변수 및 Ref 추가
+  const [receivedImage, setReceivedImage] = useState<File | null>(null)
+  const imageRef = useRef<HTMLImageElement>(null)
+
   const handleDrawButtonClick = () => {
     setDrawingMode(true)
   }
@@ -25,29 +29,6 @@ function MainPage() {
   const handleEraseButtonClick = () => {
     setDrawingMode(false)
   }
-
-  // const handleCanvasClick = () => {
-  //   if (canvasRef.current) {
-  //     // CanvasDraw 컴포넌트가 렌더링되는 DOM 요소를 캡처
-  //     const canvasElement = document.getElementById('canvas-draw')
-
-  //     if (canvasElement) {
-  //       html2canvas(canvasElement).then((canvas) => {
-  //         const drawingDataURL = canvas.toDataURL('image/png')
-
-  //         // 데이터 URL을 다운로드할 수 있는 링크를 생성합니다.
-  //         const downloadLink = document.createElement('a')
-  //         downloadLink.href = drawingDataURL
-  //         downloadLink.download = 'drawing.png'
-
-  //         // 링크를 클릭하여 다운로드를 시작합니다.
-  //         document.body.appendChild(downloadLink)
-  //         downloadLink.click()
-  //         document.body.removeChild(downloadLink)
-  //       })
-  //     }
-  //   }
-  // }
 
   const handleSubmitClick = () => {
     if (canvasRef.current) {
@@ -63,21 +44,19 @@ function MainPage() {
               const formData = new FormData()
               formData.append('drawing', blob, 'drawing.png')
 
-              // fetch API를 사용하여 POST 요청을 수행합니다.
-              fetch('http://127.0.0.1:8000/emotiart/api/upload/', {
+              console.log(formData) // 여기로 이동
+
+              fetch('http://127.0.0.1:8000/emotiart/receive-image/', {
                 method: 'POST',
-                headers: {
-                  'Content-Type': 'multipart/form-data',
-                },
                 body: formData,
               })
-                .then((response) => {
-                  // 서버 응답을 처리합니다.
-                  if (response.ok) {
-                    console.log('123')
-                  } else {
-                    console.error('456')
-                  }
+                .then((response) => response.blob())
+                .then((blob) => {
+                  // Blob을 File 객체로 변환 (현재는 확장자를 'png'로 설정)
+                  const file = new File([blob], 'receivedImage.png', { type: 'image/png' })
+
+                  // 받은 이미지를 상태에 업데이트
+                  setReceivedImage(file)
                 })
                 .catch((error) => {
                   console.error('서버에 그림을 전송하는 도중 에러가 발생했습니다:', error)
@@ -91,19 +70,40 @@ function MainPage() {
     }
   }
 
+  // 이미지가 변경될 때마다 호출되는 useEffect
+  useEffect(() => {
+    // 이미지 Ref가 존재하고 이미지 파일이 있다면 실행
+    if (imageRef.current && receivedImage) {
+      const reader = new FileReader()
+      reader.onload = () => {
+        // 이미지를 로드한 후, 이미지 Ref에 설정
+        if (imageRef.current) {
+          imageRef.current.src = reader.result as string
+        }
+      }
+      reader.readAsDataURL(receivedImage)
+    }
+  }, [receivedImage])
+
   return (
     <AppContainer>
       <Header>
         <Title>EmotiArt</Title>
         <RecommendWrap>
           <Text>recomend emoticon :</Text>
-          <Recommend>
+          {/* <Recommend>
             <img src={cat1} alt="Cat 1" />
             <img src={cat2} alt="Cat 2" />
             <img src={cat3} alt="Cat 3" />
             <img src={cat4} alt="Cat 4" />
             <img src={cat5} alt="Cat 5" />
-          </Recommend>
+          </Recommend> */}
+          {receivedImage && (
+            <Recommend>
+              {/* 이미지 Ref를 사용하여 이미지 표시 */}
+              <img ref={imageRef} alt="Received Image" />
+            </Recommend>
+          )}
         </RecommendWrap>
       </Header>
       <Container>
@@ -112,7 +112,9 @@ function MainPage() {
           <EraseButton onClick={handleEraseButtonClick} />
           <SizeSlider
             value={brushSize}
-            onChange={(e) => setBrushSize(parseInt(e.target.value, 10))}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setBrushSize(parseInt(e.target.value, 10))
+            }
           />
           <SubmitButton onClick={handleSubmitClick} />
           <AddFeelButton></AddFeelButton>
